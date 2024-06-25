a37a24e2d74880e90bce3341aa8996aa891776b5
Patrick 'Lampe' Lamprecht <Ch4s3r@users.noreply.github.com>
2024-06-25 07:35:29 +0200

    Revert "feat: Add buildpacks for building docker image"

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = (robot, { getRouter }) => {
       // endpoint to fetch github given installation rate limit
       router.get('/github-ratelimit/:installationId', githubRateLimitEndpoint(robot))
     }
+
     if (process.env.ENABLE_METRICS_ENDPOINT === 'true') {
       // expose prometheus metrics
       router.use(prometheusMiddleware())
